189ecfeec63d489e2a1a6792028d328c20e1390d
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2023-04-18 21:49:23 +0800

    update folder names

--- receta-back-end/service/CategoryService.ts
+++
@@ -1,8 +0,0 @@
-import categoryModel from "../model/CaregoryModel";
-
-export async function createCategory(newCategory: any) {
-  return await categoryModel.create({
-    image_url: newCategory.image_url,
-    name: newCategory.name,
-  });
-}
--- receta-client/src/images/Difficulty.tsx
+++
@@ -1,3 +0,0 @@
-export default function Difficulty(): JSX.Element {
-  return <></>;
-}
