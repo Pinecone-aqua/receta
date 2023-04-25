e8d495598468bcfedadbd0c4c1965e42cf41e041
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2023-04-25 23:10:44 +0800

    finish login page

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,23 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { UserService } from './users.service';
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import { UserService } from "./users.service";
 
-@Controller('users')
+@Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('get')
+  @Get("get")
   find() {
     return this.userService.allUser();
   }
 
-  // @Get('login')
-  // login(user: Object) {
-  //   return this.userService.loginUser(user);
-  // }
+  @Post("login")
+  login(@Body() user) {
+    return this.userService.loginUser(user);
+  }
 
-  @Post('create')
+  @Post("create")
   create(@Body() body: any) {
-    console.log('body: ', body);
+    console.log("body: ", body);
 
     return this.userService.createUser(body);
   }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User } from './user.schema';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "./user.schema";
 
 @Injectable()
 export class UserService {
@@ -14,7 +14,10 @@ export class UserService {
     return this.userModel.create(user);
   }
 
-  // loginUser(user: string) {
-  //   return this.userModel.findOne({  });
-  // }
+  loginUser(user) {
+    return this.userModel.findOne({
+      email: user.email,
+      password: user.password,
+    });
+  }
 }
