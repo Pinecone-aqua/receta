0bb9df21deb6e1a58e8ee95dd6c75235a0c25f95
badnaim <batnyaam.tumurbat@gmail.com>
2023-04-26 12:44:04 +0800

    some table added

---
+++ admin/src/components/icons/MenuThreeDot.tsx
@@ -0,0 +1,17 @@
+export default function MenuThreeDot() {
+  return (
+    <div>
+      <svg
+        width="88"
+        height="296"
+        viewBox="0 0 88 296"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="44" cy="44" r="44" fill="black" />
+        <circle cx="44" cy="252" r="44" fill="black" />
+        <circle cx="44" cy="148" r="44" fill="black" />
+      </svg>
+    </div>
+  );
+}
