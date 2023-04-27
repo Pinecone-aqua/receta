8de51fb1a4611925c3b5e2c826417f9828787047
enne <enne.august@gmail.com>
2023-04-28 03:06:42 +0800

    single page added

--- client/src/component/sub/RecipeCard.tsx
+++ client/src/component/sub/RecipeCard.tsx
@@ -11,14 +11,14 @@ export default function RecipeCard(props: {
     <div
       className="min-w-[180px] w-[17%] mb-10 max-h-[340px] bg-white cursor-pointer"
       onClick={() => {
-        router.push(`../recipe/${recipe._id}`);
+        router.push(`/recipe/${recipe._id}`);
       }}
     >
       <picture>
         <img
           src={`${recipe.image_url}`}
           className="w-full h-[83%] object-cover"
-          alt=""
+          alt="image"
         />
       </picture>
       <div className="flex gap-2 place-content-center">
