e723c8e23ece2e0faf9c880e021fe2651eadd12d
enne <enne.august@gmail.com>
2023-04-28 07:56:16 +0800

    added currentcolor

--- client/src/component/Layout.tsx
+++ client/src/component/Layout.tsx
@@ -1,26 +1,32 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import RecipeProvider, { useProduct } from "@/context/ProductContext";
+import { useProduct } from "@/context/ProductContext";
 import Head from "next/head";
+import { ReactNode } from "react";
 
 export default function Layout({
   children,
 }: {
-  children: JSX.Element;
+  children: ReactNode;
 }): JSX.Element {
   const { activeBtn } = useProduct();
 
   function bgHandler() {
     if (activeBtn == "difficulty") {
+      // localStorage.setItem("currentColor", "#124822");
       return "bg-[#267F40] text-white scroll-smooth duration-1000";
     } else if (activeBtn == "strong") {
+      // localStorage.setItem("currentColor", "#790C0C");
       return "bg-[#D0384C] text-white scroll-smooth duration-1000";
     } else if (activeBtn == "sweet") {
+      // localStorage.setItem("currentColor", "#B63122");
       return "bg-[#FFFBF1] scroll-smooth duration-1000 text-black";
     } else {
+      // localStorage.setItem("currentColor", "#054B68");
       return "bg-[#86A1AC] text-white scroll-smooth duration-1000";
     }
   }
+
   return (
     <div>
       <Head>
