cc4599cd91d67ec153b0710c38d876166f7a67ec
enne <enne.august@gmail.com>
2023-04-28 06:33:56 +0800

    popular component added

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "@/component/Layout";
-import Recipes from "@/component/main/Recipes";
-import Membership from "@/component/main/Membership";
-import Popular from "@/component/main/Popular";
-import Categories from "@/component/main/Categories";
-import OurStory from "@/component/main/OurStory";
-import NeedHelp from "@/component/main/NeedHelp";
+import Recipes from "@/component/sub/Recipes";
+import Membership from "@/component/sub/Membership";
+import Popular from "@/component/sub/Popular";
+import Categories from "@/component/sub/Categories";
+import OurStory from "@/component/sub/OurStory";
+import NeedHelp from "@/component/sub/NeedHelp";
+import { BsArrowDownShort } from "react-icons/bs";
 
 export default function Home(): JSX.Element {
   return (
@@ -13,22 +14,17 @@ export default function Home(): JSX.Element {
       <>
         <div className="bg-[#1E1E1E] border border-[#05445F]">
           <Categories />
-          <Recipes />
+          <Recipes bgColor="#FFFBF1" textColor="#1E1E1E" />
+          <div className="place-content-center flex my-10">
+            <div className="text-white cursor-pointer">
+              <BsArrowDownShort className="animate-bounce mx-auto w-[45px] h-[45px] border rounded-[50%]" />
+              <p className="text-[12px]">Цааш үзэх</p>
+            </div>
+          </div>
           <OurStory />
           <Membership />
         </div>
-        <div className="bg-[url(/help.png)] bg-cover bg-center h-[500px] bg-[#124822]">
-          <div className="pt-[10%] pl-[20%]">
-            <p className="text-[46px] font-bold mb-5 text-center">
-              Need help getting started?
-            </p>
-            <div className="flex justify-center">
-              <button className="font-bold border rounded-full py-2 px-[4rem] text-[24px]">
-                let us help you
-              </button>
-            </div>
-          </div>
-        </div>
+        <NeedHelp />
         <Popular />
       </>
     </Layout>
