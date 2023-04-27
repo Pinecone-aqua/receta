191c7197f984e81b21c169395eb3638728b662ea
enne <enne.august@gmail.com>
2023-04-28 05:23:18 +0800

    landing page ourStory added

---
+++ client/src/component/sub/AdviceRow.tsx
@@ -0,0 +1,10 @@
+export default function AdviceRow() {
+  return (
+    <div className="flex h-[474px]">
+      <div className="border border-s-0 border-t-0 border-[gray] w-[10%]"></div>
+      <div className="border border-s-0 border-t-0 border-[gray] w-[36%]"></div>
+      <div className="border border-s-0 border-t-0 border-[gray] w-[36%]"></div>
+      <div className="border border-s-0 border-e-0 border-[gray] border-t-0 w-[18%]"></div>
+    </div>
+  );
+}
