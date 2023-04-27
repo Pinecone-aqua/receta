3af3c08ac6b5817f8211e02b54859680c36b32af
enne <enne.august@gmail.com>
2023-04-28 06:02:24 +0800

    added some fix

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -16,8 +16,20 @@ export default function Home(): JSX.Element {
           <Recipes />
           <OurStory />
           <Membership />
-          <Popular />
         </div>
+        <div className="bg-[url(/help.png)] bg-cover bg-center h-[500px] bg-[#124822]">
+          <div className="pt-[10%] pl-[20%]">
+            <p className="text-[46px] font-bold mb-5 text-center">
+              Need help getting started?
+            </p>
+            <div className="flex justify-center">
+              <button className="font-bold border rounded-full py-2 px-[4rem] text-[24px]">
+                let us help you
+              </button>
+            </div>
+          </div>
+        </div>
+        <Popular />
       </>
     </Layout>
   );
