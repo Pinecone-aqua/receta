8559a1326afb43c06d93f050fd9608276338364e
enne <enne.august@gmail.com>
2023-04-28 06:02:24 +0800

    added some fix

---
+++ client/src/component/sub/Popular.tsx
@@ -0,0 +1,10 @@
+export default function Popular(): JSX.Element {
+  return (
+    <div className="p-5 bg-[#FFFBF1]">
+      <h2 className="container mx-auto text-3xl mb-5 font-bold">
+        Popular and easy to learn cocktails
+      </h2>
+      <div className="container mx-auto flex justify-between">dasd</div>
+    </div>
+  );
+}
