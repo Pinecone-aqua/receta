0a21f3f5140c8bdf77b424d7809badcb2fcd2131
enne <enne.august@gmail.com>
2023-04-28 04:08:07 +0800

    card style fixed

---
+++ client/src/component/sub/TilesRow.tsx
@@ -0,0 +1,12 @@
+export default function TilesRow() {
+  return (
+    <div className="flex">
+      <div className="w-[10%] h-[300px] border border-s-0"></div>
+      <div className="w-[18%] border h-[300px]"></div>
+      <div className="w-[18%] border h-[300px]"></div>
+      <div className="w-[18%] border h-[300px]"></div>
+      <div className="w-[18%] border h-[300px]"></div>
+      <div className="w-[18%] border border-e-0 h-[300px]"></div>
+    </div>
+  );
+}
