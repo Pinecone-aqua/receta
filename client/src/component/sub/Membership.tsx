032c28cf70dd8c11077313b8077f70d2f85f8db6
badnaim <batnyaam.tumurbat@gmail.com>
2023-04-28 10:02:29 +0800

    b

--- client/src/component/sub/Membership.tsx
+++ client/src/component/sub/Membership.tsx
@@ -9,7 +9,7 @@ export default function Membership(): JSX.Element {
         </h5>
         <div className="flex items-baseline text-gray-900 dark:text-white">
           <span className="text-3xl font-semibold">$</span>
-          <span className="text-5xl font-extrabold tracking-tight">49</span>
+          <span className="text-5xl font-extrabold tracking-tight">3</span>
           <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
             /month
           </span>
