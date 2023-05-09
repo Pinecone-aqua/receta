9f72f94f2f68706773f7b5e222ceb4cb884c59b3
enne <enne.august@gmail.com>
2023-05-10 07:00:56 +0800

    ourstory added

--- client/src/component/main/OurStory.tsx
+++ client/src/component/main/OurStory.tsx
@@ -1,9 +1,34 @@
+import { Carousel } from "primereact/carousel";
 import React from "react";
 
 export default function OurStory() {
+  const ourStoryValue = [
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ab porro vitae. Quasi quis impedit, qui ex reprehenderit rerum assumenda architecto dicta, veniam distinctio amet atque odio rem debitis aliquam neque eveniet eos tempora!",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ab porro vitae. Quasi quis impedit, qui ex reprehenderit rerum assumenda architecto dicta, veniam distinctio amet atque odio rem debitis aliquam neque eveniet eos tempora! part2",
+  ];
+  const ourStoryTemplate = (value: any) => {
+    return <p className="text-[18px] py-2">{value}</p>;
+  };
+  const responsiveOptions = [
+    {
+      breakpoint: "1199px",
+      numVisible: 1,
+      numScroll: 1,
+    },
+    {
+      breakpoint: "991px",
+      numVisible: 1,
+      numScroll: 1,
+    },
+    {
+      breakpoint: "767px",
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
   return (
-    <div className="bg-black text-white">
-      <div className="Container border-s border-[#424242]">
+    <div className="bg-black text-white ourstory">
+      <div className="Container">
         <h1 className="text-[72px] font-semibold px-10 py-[6rem]">
           <span className="border-b pb-3">Our</span> story
         </h1>
@@ -11,14 +36,18 @@ export default function OurStory() {
           <img src="./OurStory.png" alt="image" />
           <div className="ps-10 flex flex-col justify-between">
             <div>
-              <h1 className="text-3xl font-semibold">Write title here</h1>
-              <p className="py-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus ab porro vitae. Quasi quis impedit, qui ex
-                reprehenderit rerum assumenda architecto dicta, veniam
-                distinctio amet atque odio rem debitis aliquam neque eveniet eos
-                tempora!
-              </p>
+              <h1 className="text-4xl font-semibold">Write title here</h1>
+              <div className="mt-5">
+                <Carousel
+                  value={ourStoryValue}
+                  circular={true}
+                  numVisible={1}
+                  numScroll={1}
+                  responsiveOptions={responsiveOptions}
+                  itemTemplate={ourStoryTemplate}
+                  indicatorsContentClassName={"flex justify-center gap-0"}
+                />
+              </div>
             </div>
             <button className="py-2 px-[6rem] border me-auto">read more</button>
           </div>
