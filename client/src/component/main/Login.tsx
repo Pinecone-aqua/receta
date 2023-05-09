5758064bdebf172fcaf249c4a86d2c087ba623db
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2023-05-09 21:37:26 +0800

    leo

--- client/src/component/main/Login.tsx
+++ client/src/component/main/Login.tsx
@@ -2,17 +2,13 @@ import { useRouter } from "next/router";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import {
-  Input,
   Modal,
   ModalOverlay,
   ModalContent,
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  FormControl,
-  FormLabel,
   useDisclosure,
-  Divider,
   Tooltip,
 } from "@chakra-ui/react";
 import axios from "axios";
@@ -48,24 +44,10 @@ export default function Login() {
         <ModalContent>
           <ModalHeader>Login modal</ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>Email or name</FormLabel>
-              <Input
-                ref={initialRef}
-                placeholder="First name"
-                className="mb-[10px]"
-              />
-              <Input ref={initialRef} placeholder="Password" />
-              <button className="w-full shadow rounded-md  bg-white px-[16px] py-[6px] mt-5">
-                Login
-              </button>
-              <Divider orientation="horizontal" className="mt-6" />
-            </FormControl>
-
+          <ModalBody pb={10}>
             <span
               onClick={googleLoginHandler}
-              className="flex place-content-center cursor-pointer mt-5 px-5 p-2 bg-white shadow rounded-[25px] gap-2"
+              className="flex place-content-center cursor-pointer mt-5 mb-2 px-5 p-2 bg-white shadow shadow-[#1e1e1e] rounded-[25px] gap-2"
             >
               <p className="text-[16px]">sign in google</p>
               <FcGoogle className="mt-[3px] w-[20px] h-[20px]" />
