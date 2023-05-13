ca7228116b23524a0aecf80cfb03f34a774f343b
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2023-05-13 20:49:29 +0800

    about page create and main page response and page loading

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import UserProvider from "../context/UserContext";
 import CocktailProvider from "../context/CocktailContext";
 import React from "react";
+import Loader from "@/component/Loader";
 export default function App({
   Component,
   pageProps: { ...pageProps },
@@ -16,7 +17,9 @@ export default function App({
       <CocktailProvider>
         <OthersProvider>
           <ChakraProvider>
-            <Component {...pageProps} />
+            <Loader>
+              <Component {...pageProps} />
+            </Loader>
           </ChakraProvider>
         </OthersProvider>
       </CocktailProvider>
