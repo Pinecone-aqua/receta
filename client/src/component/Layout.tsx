d7f9ecd3dd69175fc1d027ae8f70eb218ad5e0e6
leo <munkhsvld786@gmail.com>
2023-05-18 13:49:13 +0800

    last google login commit

--- client/src/component/Layout.tsx
+++ client/src/component/Layout.tsx
@@ -5,6 +5,8 @@ import Head from "next/head";
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
+import { useUser } from "@/context/UserContext";
+import jwtDecode from "jwt-decode";
 
 export default function Layout({
   children,
@@ -12,11 +14,12 @@ export default function Layout({
   children: JSX.Element;
 }): JSX.Element {
   const router = useRouter();
+  const { setUser } = useUser();
   useEffect(() => {
     if (router.query.token) {
       Cookies.set("token", `${router.query.token}`);
       router.replace("/");
-      router.reload();
+      setUser(jwtDecode(`${router.query.token}`));
     }
   }, [router]);
 
