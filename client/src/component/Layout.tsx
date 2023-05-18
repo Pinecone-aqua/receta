9138c8def74024d4411a8232fe555ceb5b3a4dcd
leo <munkhsvld786@gmail.com>
2023-05-18 13:28:19 +0800

    last google login

--- client/src/component/Layout.tsx
+++ client/src/component/Layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Header from "./Header";
 import Footer from "./Footer";
 import Head from "next/head";
@@ -16,7 +17,7 @@ export default function Layout({
       Cookies.set("token", `${router.query.token}`);
       router.push("../");
     }
-  }, []);
+  }, [router.query.token]);
 
   return (
     <>
