338022e6808209748e798e15d6c5143fbdeaf408
Munkh Suld <munkhsuld@Munkhs-MacBook-Air.local>
2023-05-21 18:17:16 +0800

    up

--- client/src/component/Loader.tsx
+++ client/src/component/Loader.tsx
@@ -14,12 +14,6 @@ const Loader = ({ children }: { children: ReactNode }) => {
     router.events.on("routeChangeStart", handleStart);
     router.events.on("routeChangeComplete", handleComplete);
     router.events.on("routeChangeError", handleComplete);
-
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
   }, [router]);
 
   return loading ? (
